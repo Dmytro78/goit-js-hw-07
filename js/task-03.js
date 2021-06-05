34a2de8c3afffc7defa9714ded9537bad7e0d70e
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-06-05 12:22:09 +0300

    03

--- js/task-03.js
+++ js/task-03.js
@@ -24,3 +24,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryList = document.querySelector('#gallery');
+
+const galleryImages = images.map(({url, alt}) => { return `<li><img src="${url}" alt="${alt}" width = 200 height = 150><li>`});
+
+galleryList.insertAdjacentHTML("afterbegin", galleryImages);
+galleryList.setAttribute("style", "list-style-type:none; display: flex;");
