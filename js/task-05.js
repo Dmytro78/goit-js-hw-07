1b080c3b191144a8166385bde9df05566b45aedd
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-06-05 15:32:35 +0300

    05

--- js/task-05.js
+++ js/task-05.js
@@ -1,3 +1,14 @@
 /*Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), 
 подставляет его текущее значение в span#name-output. 
 Если инпут пустой, в спане должна отображаться строка 'незнакомец'.*/
+
+const input = document.getElementById("name-input");
+const nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'незнакомец';
+    } else{
+    nameOutput.innerHTML = input.value;
+    }
+}
