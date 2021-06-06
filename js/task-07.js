d09f823fff1702c5f656c1bc7e473fda09debddb
Dmitriy Liashchenko <fttravel78@gmail.com>
2021-06-06 11:19:05 +0300

    07

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,11 @@
 /*Напиши скрипт, который реагирует на изменение значения input#font - size - control(событие input) и 
 изменяет инлайн - стиль span#text обновляя свойство font - size.
 В результате при перетаскивании ползунка будет меняться размер текста.*/
+
+const inputFont = document.getElementById("font-size-control");
+
+const spanText = document.getElementById("text");
+
+inputFont.oninput = function () {
+    spanText.style.fontSize = inputFont.value + "px";
+};
